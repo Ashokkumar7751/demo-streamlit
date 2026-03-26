0c5426826337a4dc8bb00752e8caa424a4931c0b
Ashokkumar7751 <ashokg2023@gmail.com>
2026-03-26 08:01:46 +0530

    Add files via upload

---
+++ 3_.py
@@ -0,0 +1,8 @@
+import streamlit as sl
+
+sl.title("this is main page")
+sl.subheader("this is the sub header 2 from the method")
+sl.text("ifho uhfo f iof hweih foiwe ofhfoid ois ofie oiwej oifwehfi sd s ewoi ewoijeo uhfo f iof hweih foiwe ofhfoid ois ofie oiwej oifweo uhfo f iof hweih foiwe ofhfoid ois ofie oiwej oifwehfi sd s ewoio uhfo f iof hweih foiwe ofhfoid ois ofie oiwej oifwehfi sd s ewoio uhfo f iof hweih foiwe ofhfoid ois ofie oiwej oifwehfi sd s ewoio uhfo f iof hweih foiwe ofhfoid ois ofie oiwej oifwehfi sd s ewoio uhfo f iof hweih foiwe ofhfoid ois ofie oiwej oifwehfi sd s ewoio uhfo f iof hweih foiwe ofhfoid ois ofie oiwej oifwehfi sd s ewoio uhfo f iof hweih foiwe ofhfoid ois ofie oiwej oifwehfi sd s ewoihfi sd s ewoiw0ij oief iojf weuf0e ue fewu f iof hweih foiwe ofhfoid ois ofie oiwej oifwehfi sd s ewoi ewoijew0ij oief iojf weuf0e ue fewu")
+
+
+sl.header("ifho uhfo f iof hweih foiwe ofhfoid ois ofie oiwej oifwehfi sd s ewoi ewoijew0ij oief iojf weuf0e ue fewu f iof hweih foiwe ofhfoid ois ofie oiwej oifwehfi sd s ewoi ewoijew0ij oief iojf weuf0e ue fewu")
